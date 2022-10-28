cacb70ae8eef56583ed86300e942dcac6ed512c1
Aakash Patel <akashpatelp83@gmail.com>
2022-10-28 23:27:37 +0530

    add day7 hangman project main file

---
+++ 008 Day-7-Hangman/008-Day-7-Hangman.py
@@ -0,0 +1,99 @@
+print("Welcome to The Hangman Game. Guess correct letters to save the man from hanging!! ")
+import random
+
+#All the stages of hanging
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+#Choosing random word from the list
+word_list = ["mammal", "postman", "random", "lost"]
+to_guess = random.choice(word_list)
+
+#Track of correctly choosen words
+guessed_list = ["_"]*len(to_guess)
+life = 6
+
+# main logic
+end_of_game = False
+while not end_of_game:
+    user_guessing = input("\nGuess a word: ").lower()
+    end_life = True
+
+    #Check for correct guess
+    for i in range(len(to_guess)):
+        if user_guessing == to_guess[i]:
+            guessed_list[i] = user_guessing
+            end_life = False
+
+    #Check for remaining life
+    if end_life:
+        life -= 1
+        if life <= 0:
+            print("You lose !!")
+            end_of_game = True
+
+    print(f"{' '.join(guessed_list)}")
+
+    #All letters guessed
+    if "_" not in guessed_list:
+        end_of_game = True
+        print("You Won !!")
+
+    #How's The Hangman
+    print(stages[life])
+
+print("Correct word: ", to_guess)
