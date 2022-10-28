fd8d88dc26f60e59d2207c3ea2a830935fea9a78
Aakash Patel <akashpatelp83@gmail.com>
2022-10-28 23:55:53 +0530

    Formate modules

--- 008 Day-7-Hangman/hangman_words.py
+++ 008 Day-7-Hangman/hangman_words.py
@@ -1,4 +1,2 @@
-words_list = ["execute", "isolation", "walk", "home", "snail", "fraud", "committee", "precision", "paragraph", "opponent", "torch", "sniff", "veteran", "outlet", "ally", "consensus", "fan", "stimulation", "deny", "solo"]
-
-
-
+words_list = ["execute", "isolation", "walk", "home", "snail", "fraud", "committee", "precision", "paragraph", 
+"opponent", "torch", "sniff", "veteran", "outlet", "ally", "consensus", "fan", "stimulation", "deny", "solo"]
