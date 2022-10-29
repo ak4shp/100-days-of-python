af5b4ddc8da49c8582bc8912b1a94e3daa8c3147
Aakash Patel <akashpatelp83@gmail.com>
2022-10-29 18:14:35 +0530

    Modify Day 8 Caesar Cipher project
    Implemented decryption functionality

--- 009_Day_8_Caesar_Cipher_Encoder.py
+++ 009_Day_8_Caesar_Cipher_Encoder.py
@@ -1,26 +1,36 @@
 from string import ascii_lowercase as abc
 
-
 alphabates = abc
-def encrypt(text, shift):
+
+def encrypt(plain_text, to_shift):
     encrypted = ''
-    for character in text:
+    for character in plain_text:
         if character not in alphabates:
             encrypted += character
         else:
             idx = alphabates.index(character)
-            new_idx = (idx + shift) % 26
+            new_idx = (idx + to_shift) % 26
             new_character = alphabates[new_idx]
-            encrypted += new_character
-            
+            encrypted += new_character         
     print("The encoded text is ", encrypted)
 
 
-def decrypt():
-    pass
+def decrypt(encrypted_text, to_shift_back):
+    decrypted = ''
+    for character in encrypted_text:
+        if character not in alphabates:
+            decrypted += character
+        else:
+            idx = alphabates.index(character)
+            new_idx = idx - to_shift_back
+            new_character = alphabates[new_idx]
+            decrypted += new_character
+    print("Msg is ", decrypted)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+direction = input("Choose (1) to encrypt, type (0) to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt(text, shift)
+decrypt(encrypted_text = text, to_shift_back=shift)
+
