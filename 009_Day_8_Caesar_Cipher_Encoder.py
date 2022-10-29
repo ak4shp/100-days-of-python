598a29cbf496be674028e0b6bb44c3a4f72c44c1
Aakash Patel <akashpatelp83@gmail.com>
2022-10-29 19:15:30 +0530

    Modify Day 8 Caesar Cipher
    Optimization and added main_menu for restarting program without quitting.

--- 009_Day_8_Caesar_Cipher_Encoder.py
+++ 009_Day_8_Caesar_Cipher_Encoder.py
@@ -3,65 +3,32 @@ from string import ascii_lowercase as abc
 alphabates = abc
 
 def cipher(to_cipher_text, to_shift, enc_dec):
-    encrypted = ''
-    decrypted = ''
+    cipher_text = ''
+    if enc_dec == "decode":
+        to_shift *= -1
+
     for letter in to_cipher_text:
         if letter not in alphabates:
-            encrypted += letter
-            decrypted += letter
+            cipher_text += letter
         else:
             idx = alphabates.index(letter)
             new_idx = (idx + to_shift) % 26
-            encrypted += alphabates[new_idx]   
-            new_idx2 = idx - to_shift
-            decrypted += alphabates[new_idx2]
-
-    if enc_dec == '1':
-        print("The encoded text is ", encrypted)
-    elif enc_dec == '0':    
-        print("The decoded text is ", decrypted)
-    else:    
-        print("Invalid!! Choose (1) to encryt or (0) to decrypt.")
+            cipher_text += alphabates[new_idx]   
+    print(f"The {enc_dec}d text is --> {cipher_text} <--")
 
 
 # MAIN 
-direction = input("Choose (1) to encrypt, type (0) to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def main_menu():
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    cipher(to_cipher_text=text, to_shift=shift, enc_dec=direction)
 
-cipher(to_cipher_text=text, to_shift=shift, enc_dec=direction)
-
+    again = input("\nType 'yes' to restart, type 'no' to exit: ").lower()
+    if again == "yes":
+        main_menu()
+    else:
+        print("End of Caesar Cipher !!")
             
 
-# def encrypt(plain_text, to_shift):
-#     encrypted = ''
-#     for character in plain_text:
-#         if character not in alphabates:
-#             encrypted += character
-#         else:
-#             idx = alphabates.index(character)
-#             new_idx = (idx + to_shift) % 26
-#             new_character = alphabates[new_idx]
-#             encrypted += new_character         
-#     print("The encoded text is ", encrypted)
-
-
-# def decrypt(encrypted_text, to_shift_back):
-#     decrypted = ''
-#     for character in encrypted_text:
-#         if character not in alphabates:
-#             decrypted += character
-#         else:
-#             idx = alphabates.index(character)
-#             new_idx = idx - to_shift_back
-#             new_character = alphabates[new_idx]
-#             decrypted += new_character
-#     print("The decoded text is ", decrypted)
-
-
-# if direction == '1':
-#     encrypt(plain_text = text, to_shift = shift)
-# elif direction == '0':
-#     decrypt(encrypted_text = text, to_shift_back=shift)
-# else:
-#     print("Invalid!! Choose 1 or 0")
+main_menu()
