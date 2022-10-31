22212632eb7cb824d0fc262d59db1ca487e031e8
Aakash Patel <akashpatelp83@gmail.com>
2022-10-31 20:10:31 +0530

    Modify Day 10 Calculator project
    Implemented continuing calculation

--- 014_Day_10_Calculator/014_Day_10_Calculator.py
+++ 014_Day_10_Calculator/014_Day_10_Calculator.py
@@ -29,27 +29,20 @@ num1 = int(input("First number? "))
 for symbol in operations:
     print(symbol)
 
-operation_symbol = input("\nWhich operation?  ")
-num2 = int(input("Next number? "))
-calculation_function = operations[operation_symbol] #Uses dict's key|value to do asked operation
-ans = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {ans}")
+# Continuing calculation
+more_calculation = True
+while more_calculation:
+    operation_symbol = input("\nWhich operation?  ")
+    num2 = int(input("Next number? "))
+    calculation_function = operations[operation_symbol] #Uses dict's key|value to do asked operation
+    ans = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {ans}")
+    num1 = ans
 
-
-# # Continuing calculation
-# more_calculation = True
-# while more_calculation:
-#     operation_symbol = input("\nWhich operation?  ")
-#     num2 = int(input("Next number? "))
-#     calculation_function = operations[operation_symbol] #Uses dict's key|value to do asked operation
-#     ans = calculation_function(num1, num2)
-#     print(f"{num1} {operation_symbol} {num2} = {ans}")
-#     num1 = ans
-
-#     more_operation = input(f"Type 'y' to continue calculating with {ans}, or press any key to exit.: ").lower()
-#     if more_operation == 'y':
-#         continue
-#     else:
-#         print("Final result is: ", ans)
-#         more_calculation = False    
+    more_operation = input(f"Type 'y' to continue calculating with {ans}, or press any key to exit.: ").lower()
+    if more_operation == 'y':
+        continue
+    else:
+        print("Final result is: ", ans)
+        more_calculation = False    
     
