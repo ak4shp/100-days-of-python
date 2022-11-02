888efd2fa3a9540522ded60ddacc7c05f39585b5
Aakash Patel <akashpatelp83@gmail.com>
2022-11-02 21:58:24 +0530

    Refactor: Day 12 Number Guessing Project
    Add play_game() function and checking loop for correct answer.

--- 017_Day_12_Number_Guessing_Game/017_Day_12_Number_Guessing_Game.py
+++ 017_Day_12_Number_Guessing_Game/017_Day_12_Number_Guessing_Game.py
@@ -3,8 +3,6 @@ from Number_Guessing_Game_art import logo
 
 welcome = """Welcome to the number guessing game!
 I'm thinking of a number between 1 and 100. Can you guess it?"""
-print(logo)
-print(welcome)
 
 NUMBER = random.randint(1, 100)
 EASY_LEVEL_TURNS = 10
@@ -12,6 +10,7 @@ MEDIUM_LEVEL_TURN = 7
 HARD_LEVEL_TURN = 4
 
 def set_difficulty():
+    '''Returns total attempts(turns) based on user chosen difficulty level.'''
     difficulty_level = input("Choose a difficulty. Type 'e' for easy, 'm' for moderate, 'h' for hard.: ").lower()
     if difficulty_level == 'e':
         return EASY_LEVEL_TURNS
@@ -21,24 +20,30 @@ def set_difficulty():
         return HARD_LEVEL_TURN
      
 
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
+    '''Checks for correct answer. Returns remaining attempts(turns).'''
     if guess > answer:
         print("Too high. Try again.")
+        return turns - 1
     elif guess < answer:
         print("Too low. Try again!")
+        return turns - 1
     else:
         print("You got it! The answer is ", answer)
 
 
-attempts = set_difficulty()
-print(NUMBER)
-while attempts > 0:
-    print(f"\nYou have {attempts} attempts remaining to guess the number.") 
-    guess_number = int(input("Make a guess: "))
-    check_answer(guess=guess_number, answer=NUMBER)
-    if guess_number == NUMBER:
-        break
-    attempts -= 1
-    if attempts == 0:
-        print("Oops! No more guesses are allowed. You lose.")
+def play_game():
+    print(logo)
+    print(welcome)
 
+    attempts = set_difficulty()
+    guess_number = 0
+    while guess_number != NUMBER:
+        print(f"\nYou have {attempts} attempts remaining to guess the number.") 
+        guess_number = int(input("Make a guess: "))
+        attempts = check_answer(guess=guess_number, answer=NUMBER, turns=attempts)
+        if attempts == 0:
+            print("Oops! No more guesses are allowed. You lose.")
+            return 
+
+play_game()
