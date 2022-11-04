86c349a2e7515f1486121c50fb78f1d3187c2e57
Aakash Patel <akashpatelp83@gmail.com>
2022-11-05 03:22:39 +0530

    Modify: Day 13 debugging 2 file
    Debugged leap-year program

--- 019_Day_13_Debugging_2.py
+++ 019_Day_13_Debugging_2.py
@@ -8,5 +8,18 @@ else:
   print("This is an odd number.")
 
 
+# Leap-year : fix the bug
   
+year = int(input("Which year do you want to check?"))
+#Bug: year was string, casted to int.
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
   
