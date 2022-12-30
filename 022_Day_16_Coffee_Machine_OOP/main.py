360d532269609c5c286d246c1856812baf650eac
Aakash Patel <akashpatelp83@gmail.com>
2022-12-31 01:26:49 +0530

    Modify: Day 16 project add logo

--- 022_Day_16_Coffee_Machine_OOP/main.py
+++ 022_Day_16_Coffee_Machine_OOP/main.py
@@ -1,11 +1,12 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from art import LOGO
 
 
 order = CoffeeMaker()
 money = MoneyMachine()
-
+print(LOGO)
 is_on = True
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino/) or (off/report): ")
