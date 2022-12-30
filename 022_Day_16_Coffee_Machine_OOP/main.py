a74e0e26c062477bef94af68330280c36db2fb93
Aakash Patel <akashpatelp83@gmail.com>
2022-12-31 01:20:47 +0530

    Add Day 16 Coffee Machine OOP project

---
+++ 022_Day_16_Coffee_Machine_OOP/main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+order = CoffeeMaker()
+money = MoneyMachine()
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino/) or (off/report): ")
+    if choice == "report":
+        order.report()
+        money.report()
+
+    elif choice == "off":
+        is_on = False
+
+    else:
+        drink = Menu()
+        drink_name = drink.find_drink(choice)
+
+        if drink_name and order.is_resource_sufficient(drink_name):
+            drink_cost = drink_name.cost
+            if money.make_payment(drink_cost):
+                order.make_coffee(drink_name)
+
+
