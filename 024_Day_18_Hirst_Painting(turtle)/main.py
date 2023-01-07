8802a7e4cbab0d3f7e0741f38ae4abc3e7123f9a
Aakash Patel <akashpatelp83@gmail.com>
2023-01-08 04:05:51 +0530

    Modify: Day 18 Hirst Painting(turtle) Random colored walk

--- 024_Day_18_Hirst_Painting(turtle)/main.py
+++ 024_Day_18_Hirst_Painting(turtle)/main.py
@@ -1,18 +1,37 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 t = Turtle()
-colors = ["Black", "CornflowerBlue", "Goldenrod", "Blue", "Red", "SpringGreen", "DodgerBlue", "Magenta"]
+colormode(255)
+
+
+"""Make colored random walk"""
+def new_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+direction = [0, 90, 180, 270]
+t.pensize(5)
+t.speed("fastest")
+# t.screen.screensize(20, 30)
+for _ in range(500):
+    col = new_color()
+    t.color(col)
+    t.setheading(random.choice(direction))
+    t.forward(20)
+
 
 """Draw different shapes (nested)"""
-
-for i in range(8):
-    shape_id = i + 3
-    angle = 360 // (shape_id)
-    t.pensize(2)
-    t.color(colors[i])
-    for _ in range(shape_id):
-        t.forward(70)
-        t.right(angle=angle)
+# colors = ["Black", "CornflowerBlue", "Goldenrod", "Blue", "Red", "SpringGreen", "DodgerBlue", "Magenta"]
+# for i in range(8):
+#     shape_id = i + 3
+#     angle = 360 // (shape_id)
+#     t.pensize(2)
+#     t.color(colors[i])
+#     for _ in range(shape_id):
+#         t.forward(70)
+#         t.right(angle=angle)
 
 
 """Square with dashed line and dot"""
@@ -25,23 +44,6 @@ for i in range(8):
 #         t.dot()
 #         t.forward(5)
 #     t.dot()
-
-
-"""Make colored random drawing"""
-# colors = ["OrangeRed", "MediumSpringGreen", "DodgerBlue", "LightGreen", "HotPink"]
-# direction = [0, 90, 180, 270]
-# direction2 = [90, 180, 270]
-
-# t.pensize(2)
-# # t.speed("fastest")
-# t.screen.screensize(200, 300)
-# # for _ in range(500):
-# #     t.color(random.choice(colors))
-# #     if t.ycor() >= 300 or t.xcor() >= 200:
-# #         t.setheading(random.choice(direction2))
-# #     else:
-# #         t.setheading(random.choice(direction))
-# #     t.forward(20)
 
 
 """Draw a star filled with colors"""
