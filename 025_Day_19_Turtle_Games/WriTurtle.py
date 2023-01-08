5577f53aa98501a59d3009b5886cd3fe882f2584
Aakash Patel <akashpatelp83@gmail.com>
2023-01-09 01:46:22 +0530

    Update: Day 19 Turtle Games project-1 Key nevigation, game 'start' function to initialise objects.

--- 025_Day_19_Turtle_Games/WriTurtle.py
+++ 025_Day_19_Turtle_Games/WriTurtle.py
@@ -1,88 +1,112 @@
 from turtle import Turtle, Screen
 import random
 
-t = Turtle()
-screen = Screen()
-
 # Key Press Functions
 class Movements:
-    def __init__(self) -> None:
+    def __init__(self, turtle, screen) -> None:
+        self.t = turtle
+        self.screen = screen
         self.pen_size = 1
         self.pen_color = "Black"
 
     def fwd(self):        
         """10 step forward | key= 'Up' arrow."""
-        t.forward(10)
+        self.t.forward(10)
 
     def bkwd(self): 
         """10 step backward | key= 'Down' arrow."""
-        t.backward(10)
+        self.t.backward(10)
 
     def lft(self):
         """Turn left by 10 degree | key= 'Left' arrow."""
-        t.left(10)
+        self.t.left(10)
 
     def rght(self):
         """Turn right by 10 degree | key= 'Right' arrow."""
-
-        t.right(10)
+        self.t.right(10)
 
     def pen_size_incr(self):
         """Increase pen stroke by 1. Max upto 10 | key= 'a'"""
         # global pen_size
         if not self.pen_size >= 10:
             self.pen_size += 1
-            t.pensize(self.pen_size)
+            self.t.pensize(self.pen_size)
 
     def pen_size_decr(self):
         """Decrease pen stroke by 1. Min upto 1 | key= 's'"""
         # global pen_size
         if not self.pen_size <= 1 :
             self.pen_size -= 1
-            t.pensize(self.pen_size)
+            self.t.pensize(self.pen_size)
 
     def change_color(self):
         """Change pen stroke color randomly | Key= 'c'."""
         colors = ["Black", "CornflowerBlue", "Goldenrod", "Blue", "Red", "SpringGreen", "DodgerBlue", "Magenta"]
         self.pen_color = random.choice(colors)
-        t.color(self.pen_color)
+        self.t.color(self.pen_color)
 
     def erasor(self):
         """Erasor (Change pen stroke color to White) | Key= 'e'."""
-        t.color("White")
+        self.t.color("White")
 
     def pen_up(self):
         """Free hand mode for movement of cursor | Key= 'f'."""
-        t.penup()
+        self.t.penup()
 
     def pen_down(self):
         """Writing mode | Key= 'd'."""
-        t.pendown()
+        self.t.pendown()
 
     def clear_screen(self):
         """Clears all drawing | key= 'q'."""
-        screen.clear()
+        self.screen.clear()
+
+
+def start():
+    """Initialises objects"""
+    t = Turtle()
+    screen = Screen()
+    movement = Movements(turtle=t, screen=screen)
+    return t, screen, movement
 
 
 # Key press Events
 def main():
     # t.write("WriTurtle", font= ('Arial',12,'bold'))
-    opr = Movements()
-    t.color(opr.pen_color)
-    t.pensize(opr.pen_size)
-    screen.onkeypress(opr.fwd, "Up")
-    screen.onkeypress(opr.bkwd, "Down")
-    screen.onkeypress(opr.lft, "Left")
-    screen.onkeypress(opr.rght, "Right")
-    screen.onkeypress(opr.pen_size_incr, "a")
-    screen.onkeypress(opr.pen_size_decr, "s")
-    screen.onkeypress(opr.change_color, "c")
-    screen.onkeypress(opr.erasor, "e")
-    screen.onkeypress(opr.pen_up, "f")
-    screen.onkeypress(opr.pen_down, "d")
-    screen.onkeypress(opr.clear_screen, "q")
+    t, screen, opration = start()
+    t.color(opration.pen_color)
+    t.pensize(opration.pen_size)
+    screen.onkeypress(opration.fwd, "Up")
+    screen.onkeypress(opration.bkwd, "Down")
+    screen.onkeypress(opration.lft, "Left")
+    screen.onkeypress(opration.rght, "Right")
+    screen.onkeypress(opration.pen_size_incr, "a")
+    screen.onkeypress(opration.pen_size_decr, "s")
+    screen.onkeypress(opration.change_color, "c")
+    screen.onkeypress(opration.erasor, "e")
+    screen.onkeypress(opration.pen_up, "f")
+    screen.onkeypress(opration.pen_down, "d")
+    screen.onkeypress(opration.clear_screen, "q")
 
     screen.listen()
-
+    screen.exitonclick()    
 main()
-screen.exitonclick()
+
+
+"""
+************ Navigation ************
+
+(OPERATION --> KEY)
+Forward --> "Up" 
+Backward --> "Down" 
+Turn Left --> "Left" 
+Turn Right--> "Right" 
+Increase Pen stroke --> "a" 
+Decrease Pen stroke --> "s" 
+Change Pen stroke color --> "c" 
+Erasor --> "e" 
+Turn Off stroke --> "f" 
+Turn On stroke --> "d" 
+Clear all drawing(Beta) --> "q" 
+...
+"""
