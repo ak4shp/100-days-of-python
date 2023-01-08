ecdf5ac5f709e35ec94860454fa4049c527a6af1
Aakash Patel <akashpatelp83@gmail.com>
2023-01-09 01:01:07 +0530

    Add: Day 19 Turtle Games project-1 Start 'WriTurtle' . Implement move, turn.

--- 025_Day_19_Turtle_Games/WriTurtle.py
+++ 025_Day_19_Turtle_Games/WriTurtle.py
@@ -4,85 +4,62 @@ import random
 t = Turtle()
 screen = Screen()
 
+# Global Variables
+pen_size = 1
+pen_color = "Black"
+
+t.color(pen_color)
+t.pensize(pen_size)
+
 # Key Press Functions
-class Movements:
-    def __init__(self) -> None:
-        self.pen_size = 1
-        self.pen_color = "Black"
+def fwd():        
+    """10 step forward | 'Up' arrow."""
+    t.forward(10)
 
-    def fwd(self):        
-        """10 step forward | key= 'Up' arrow."""
-        t.forward(10)
+def bkwd(): 
+    """10 step backward | key= 'Down' arrow."""
+    t.backward(10)
 
-    def bkwd(self): 
-        """10 step backward | key= 'Down' arrow."""
-        t.backward(10)
+def lft():
+    """Turn left by 10 degree | key= 'Left' arrow."""
+    t.left(10)
 
-    def lft(self):
-        """Turn left by 10 degree | key= 'Left' arrow."""
-        t.left(10)
+def rght():
+    """Turn right by 10 degree | key= 'Right' arrow."""
 
-    def rght(self):
-        """Turn right by 10 degree | key= 'Right' arrow."""
+    t.right(10)
 
-        t.right(10)
+# def pen_size_incr():
+#     """Increase pen stroke by 1. Max upto 10 | key= 'a'"""
+#     global pen_size
+#     if not pen_size >= 10:
+#         pen_size += 1
+#         t.pensize(pen_size)
 
-    def pen_size_incr(self):
-        """Increase pen stroke by 1. Max upto 10 | key= 'a'"""
-        # global pen_size
-        if not self.pen_size >= 10:
-            self.pen_size += 1
-            t.pensize(self.pen_size)
 
-    def pen_size_decr(self):
-        """Decrease pen stroke by 1. Min upto 1 | key= 's'"""
-        # global pen_size
-        if not self.pen_size <= 1 :
-            self.pen_size -= 1
-            t.pensize(self.pen_size)
+# def pen_size_decr():
+#     """Decrease pen stroke by 1. Min upto 1 | key= 's'"""
+#     global pen_size
+#     if not pen_size <= 1 :
+#         pen_size -= 1
+#         t.pensize(pen_size)
 
-    def change_color(self):
-        """Change pen stroke color randomly | Key= 'c'."""
-        colors = ["Black", "CornflowerBlue", "Goldenrod", "Blue", "Red", "SpringGreen", "DodgerBlue", "Magenta"]
-        self.pen_color = random.choice(colors)
-        t.color(self.pen_color)
-
-    def erasor(self):
-        """Erasor (Change pen stroke color to White) | Key= 'e'."""
-        t.color("White")
-
-    def pen_up(self):
-        """Free hand mode for movement of cursor | Key= 'f'."""
-        t.penup()
-
-    def pen_down(self):
-        """Writing mode | Key= 'd'."""
-        t.pendown()
-
-    def clear_screen(self):
-        """Clears all drawing | key= 'q'."""
-        screen.clear()
-
+# def change_color():
+#     """Change pen stroke color randomly | Key= 'c'."""
+#     global pen_color
+#     colors = ["Black", "CornflowerBlue", "Goldenrod", "Blue", "Red", "SpringGreen", "DodgerBlue", "Magenta"]
+#     pen_color = random.choice(colors)
+#     t.color(pen_color)
 
 # Key press Events
-def main():
-    # t.write("WriTurtle", font= ('Arial',12,'bold'))
-    opr = Movements()
-    t.color(opr.pen_color)
-    t.pensize(opr.pen_size)
-    screen.onkeypress(opr.fwd, "Up")
-    screen.onkeypress(opr.bkwd, "Down")
-    screen.onkeypress(opr.lft, "Left")
-    screen.onkeypress(opr.rght, "Right")
-    screen.onkeypress(opr.pen_size_incr, "a")
-    screen.onkeypress(opr.pen_size_decr, "s")
-    screen.onkeypress(opr.change_color, "c")
-    screen.onkeypress(opr.erasor, "e")
-    screen.onkeypress(opr.pen_up, "f")
-    screen.onkeypress(opr.pen_down, "d")
-    screen.onkeypress(opr.clear_screen, "q")
+screen.onkeypress(fwd, "Up")
+screen.onkeypress(bkwd, "Down")
+screen.onkeypress(lft, "Left")
+screen.onkeypress(rght, "Right")
+# screen.onkeypress(pen_size_incr, "a")
+# screen.onkeypress(pen_size_decr, "s")
+# screen.onkeypress(change_color, "c")
 
-    screen.listen()
+screen.listen()
 
-main()
 screen.exitonclick()
