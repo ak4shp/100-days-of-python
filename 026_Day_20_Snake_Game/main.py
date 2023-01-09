034051265fa826cba00bae92fe134d1fe8378a29
Aakash Patel <akashpatelp83@gmail.com>
2023-01-10 00:50:29 +0530

    Modify: Day 20 Snake Game Todo 1- Make snake's body and setup screen.

--- 026_Day_20_Snake_Game/main.py
+++ 026_Day_20_Snake_Game/main.py
@@ -1,5 +1,5 @@
+from turtle import Turtle, Screen
 
-#TODO 1: Create a snake body
 #TODO 2: Move the snake
 #TODO 3: Control the snake by arraw keys
 #TODO 4: Detect collision with food
@@ -7,5 +7,19 @@
 #TODO 6: Detect collision with wall
 #TODO 7: Detect Collision with tail
 
+#* Setup Screen 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Real Python! Hishhhh...")
 
+#TODO 1: Create a snake body
+x_pos = 0
+for i in range(3):
+    t= Turtle(shape="square")
+    t.penup()
+    t.color("white")
+    t.goto(x=x_pos, y=0)
+    x_pos -= 20
 
+screen.exitonclick()
